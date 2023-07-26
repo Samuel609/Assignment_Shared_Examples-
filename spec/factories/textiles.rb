03d34e866806e80592bc9bc2a5df796381690027
Samuel Fita <samabera03@gmail.com>
2023-07-26 23:19:02 +0300

    migration done on models

---
+++ spec/factories/textiles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :textile do
+    name { "MyString" }
+    price { 1.5 }
+    status { "MyString" }
+    description { "MyText" }
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    age { 1 }
+    gender { false }
+  end
+end
